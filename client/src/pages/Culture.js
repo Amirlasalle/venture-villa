7a3cdd78a516705d5b0ed90fbdf511e80165202d
アミール　モハメド <mariemosalca@Amirs-MacBook.fios-router.home>
2023-10-22 11:28:11 -0400

    added carousel slides to culture page.

--- client/src/pages/Culture.js
+++ client/src/pages/Culture.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "../index.css"
-import { Image, Card, Carousel, Container } from 'react-bootstrap';
+import { Row, Col, Image, Card, Carousel, Container } from 'react-bootstrap';
 // import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
@@ -12,75 +12,75 @@ const Culture = () => {
 
 
     const nextIconStyle = {
-        fontSize: '10vw',
+        fontSize: '3rem',
         fontWeight: 'bolder',
     };
 
     return (
-        <div className='article-slides mt-5'>
+        <Container id="root" className='d-flex flex-wrap justify-content-around mt-4 pt-5 about' >
 
-            <Container className='article-slides-container'>
-        
-
-             
-            
-                        <Carousel slide={false} nextIcon={<FontAwesomeIcon icon={faChevronRight} />} style={nextIconStyle}
-                            prevIcon={<FontAwesomeIcon icon={faChevronLeft} />} className='about'>
-                                <div className='content-container'>
-                            <Carousel.Item className="content-container">
-
-                                <Carousel className='mb-5'>
-
-                                    <Carousel.Item className="carousel-item-one">
-                                        <Image src={process.env.PUBLIC_URL + "/assets/short/tokyo-bannershort.jpeg"} className="img-fluid d-flex flex-wrap justify-content-center slide-image" />
-                                    </Carousel.Item>
-                                </Carousel>
-                                <div className="carousel-two">
-                                    <Carousel className='mt-5 about'>
-                                        <div className="carousel-item-two">
-                                        <Carousel.Item className="carousel-item-two">
-                                            <Carousel.Caption className='d-flex flex-column justify-content-around text-center'>
-                                                <h3 className='bg-white'>In Cartagena Every Day Feels Like a Fiesta!</h3>
-
-                                            </Carousel.Caption>
-                                        </Carousel.Item>
-                                        </div>
-                                    </Carousel>
-                                </div>
-                            </Carousel.Item>
-                            </div>
-                            {/* <Carousel.Item className="carousel">
-
-                                <Carousel className='mb-5'>
-
-                                    <Carousel.Item className="carousel-item-one">
-
-                                        <Image src={process.env.PUBLIC_URL + "/assets/short/cartagenashort.jpeg"} className="img-fluid d-flex flex-wrap justify-content-center slide-image" />
-                                    </Carousel.Item>
-                                </Carousel>
-                                <Carousel className='mt-5'>
-                                    <Carousel.Item className="carousel-item-two">
-                                        <Image src={process.env.PUBLIC_URL + "/assets/short/tokyo-bannershort.jpeg"} className="img-fluid d-flex flex-wrap justify-content-center" />
-                                    </Carousel.Item>
-                                </Carousel>
-
-                            </Carousel.Item> */}
-                        </Carousel>
+            <Row>
 
 
+                <Carousel slide={false} nextIcon={<FontAwesomeIcon icon={faChevronRight} />} style={nextIconStyle}
+                    prevIcon={<FontAwesomeIcon icon={faChevronLeft} />} className='about'>
+                    <Carousel.Item className="carousel-two">
 
-                <Card></Card>
+                        <div>
+                            <Row className='mt-4  about'>
+                                <Image src={process.env.PUBLIC_URL + "/assets/short/antioquiashort.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around page-img" />
+                                <h2 className="mt-3 mb-2 pl-5 pr-5 justify-content-center about-asco-text about">Colombia, a country long associated with a turbulent past, has emerged as a top destination for travelers seeking unique experiences, rich cultural heritage, and stunning 
+                                </h2>
+                                <p className="mt-1 mb-3 pl-5 pr-5 justify-content-center about-asco-text-smaller about">Colombia, a country long associated with a turbulent past, has emerged as a top destination for travelers 
+                                </p>
+                                <h2 className="text-center p-3 mt-2 section-divider-y w-100" >
+                                </h2>
+                                <h2 className="text-center section-divider-b w-100" >
+                                </h2>
+                                <h2 className="text-center section-divider-r w-100" >
+                                </h2>
+
+                                <h3 className="mt-3 pl-5 pr-5 justify-content-center about-asco-text about">Where Cultural Richness, Breathtaking Landscapes, Culinary Delights, o Create an Unforgettable Journey
+                                </h3>
 
 
+                            </Row>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item className="carousel-two">
+
+                        <div>
+                            <Row className='mt-4  about'>
+                                <Image src={process.env.PUBLIC_URL + "/assets/short/cartagenashort.jpeg"} className="img-fluid d-flex flex-wrap justify-content-around page-img" />
+                                <h2 className="mt-3 mb-2 pl-5 pr-5 justify-content-center about-asco-text about">Discover Colombia: A Gem of South America Worth Exploring
+                                </h2>
+                                <p className="mt-1 mb-3 pl-5 pr-5 justify-content-center about-asco-text-smaller about">Colombia, a country long associated with a turbulent past, has emerged as a top destination for travelers seeking unique experiences, rich cultural heritage, and stunning natural beauty. This diverse South American nation boasts a wealth of reasons why you should add it to your travel bucket list.
+                                </p>
+                                <h2 className="text-center p-3 mt-2 section-divider-y w-100" >
+                                </h2>
+                                <h2 className="text-center section-divider-b w-100" >
+                                </h2>
+                                <h2 className="text-center section-divider-r w-100" >
+                                </h2>
+
+                                <h3 className="mt-3 pl-5 pr-5 justify-content-center about-asco-text about">Where Cultural Richness, Breathtaking Landscapes, Culinary Delights, and Colombian Love Converge to Create an Unforgettable Journey
+                                </h3>
 
 
-
-  
-</Container>
+                            </Row>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
 
 
 
-        </div>)
+
+
+
+            </Row>
+        </Container >
+
+    )
 
 }
 export default Culture;

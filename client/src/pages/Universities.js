d439aa11732345f9e7740046fbbeba15050765e2
アミール　モハメド <mariemosalca@Amirs-MacBook.fios-router.home>
2023-10-28 02:07:08 -0400

    added universities to uni-cards.

--- client/src/pages/Universities.js
+++ client/src/pages/Universities.js
@@ -8,51 +8,48 @@
 // // import Tab from 'react-bootstrap/Tab';
 // // import Tabs from 'react-bootstrap/Tabs';
 // // import Button from 'react-bootstrap/Button';
-// // import Modal from 'react-bootstrap/Modal';
+// // import Modal from 'react-bootstrap/Modal';, Card, Row 
 
 import { useState } from 'react';
 import universitiesData from "../components/Jsons/universities.json";
-import { Image, Container, Col, Card, Row } from 'react-bootstrap';
+import { Image, Container, Col} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const Universities = () => {
-  const [universities] = useState(universitiesData);
+    const [universities] = useState(universitiesData);
 
-  return (
-    <div className="home">
-      <Container fluid secondary="true" className="d-flex flex-wrap justify-content-around home">
-        <Row className='mt-5'>
-          <div id="homepageCards" className="d-flex flex-wrap justify-content-center uni-btn align-items-center">
-            {universities.map((university, key) => (
-                <a href={university.uniUrl} target="_blank" rel="noreferrer" className="uni-btn">
-              <Card
-                key={key}
-                className="m-5 university-card" style={{ width: '30rem', height: '10rem' }}>
-                  <Card.Body className="d-flex justify-content-center align-items-center">
-                    <Col xs={2} md={2} className='uni-img'>
-                      <Image src={process.env.PUBLIC_URL + university.logo} className="img-fluid uni-img" thumbnail />
-                    </Col>
-                    <div className="text-center">
-                      <Card.Title>
-                        <h6 className='pl-0 uni-name'>
-                          {university.name}
-                        </h6>
-                      </Card.Title>
-                      <Card.Text className="pl-0 uni-brief">
-                        <p className='pl-0 uni-brief'>{university.brief}</p>
-                      </Card.Text>
-                    </div>
-                    <Card.Subtitle className="mb-2 mr-1 uni-fcr"><span className='text-muted fcr'><FontAwesomeIcon icon={faChevronRight} size="lg" /></span></Card.Subtitle>
-                  </Card.Body>
-              </Card>
-                </a>
-            ))}
-          </div>
-        </Row>
-      </Container>
-    </div>
-  );
+    return (
+
+        <div className="uni-main mt-5 mb-5">
+            <Container fluid secondary="true" className=" d-flex flex-wrap uni-main">
+                {universities.map((university, key) => (
+
+                    <button key={key} className="document-category" style={{ width: '25rem', height: '7rem' }}>
+                        <a href={university.uniUrl} target="_blank" rel="noreferrer" className="no-decoration">
+                            <div className="document-category-container">
+                                <div className="document-category-item document-icon-title">
+                                    <Col xs={3} md={3} className='justify-content-start ml-1 icon'>
+                                        <Image className="img-fluid icon-medium hydrated" no-hover="true" src={process.env.PUBLIC_URL + university.logo}   style={{ width: '50px', height: '50px', border: 'none' }}thumbnail />
+                                    </Col>
+
+
+                                    <div className="document-title">
+                                        <span className="document-cat-name">{university.name}</span>
+                                        <p className="pl-0 document-category-desc">{university.brief}</p>
+                                    </div>
+                                </div>
+                                <span className="document-category-item document-count">
+                                    <FontAwesomeIcon icon={faChevronRight} size="lg" /></span>
+                            </div>
+                        </a>
+                    </button>
+
+                ))}
+            </Container>
+        </div>
+
+    );
 }
 
 export default Universities;

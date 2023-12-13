349e34c5b409dbeeab0d69cb5aa6421d71ad82cf
アミール　モハメド <mariemosalca@Amirs-MacBook.fios-router.home>
2023-12-13 18:52:38 -0500

    finished restaurant api.

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,26 +1,76 @@
-import React from 'react'
-import GoogleMapReact from 'google-map-react'
+// import React from 'react'
+// import GoogleMapReact from 'google-map-react'
+// // import { Image } from 'react-bootstrap'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faLocation } from '@fortawesome/free-solid-svg-icons';
 
-const Map = ({ setCoordinates, setBounds, coordinates}) => {
+// const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
 
+
+//   return (
+//     <div className='map-container'>
+//       <GoogleMapReact
+//         bootstrapURLKeys={{ key: 'AIzaSyDzy3wz5yxKZv9CaxbAHUstxBLvan78zsY' }}
+//         defaultCenter={coordinates}
+//         center={coordinates}
+//         defaultZoom={14}
+//         margin={[50, 50, 50, 50]}
+//         options={{ fullscreenControl: false }}
+//         onChange={(e) => {
+//           setCoordinates({ lat: e.center.lat, lng: e.center.lng })
+//           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+//         }}
+//         onChildClick={''}
+//       >
+//         {places?.map((place, key) => (
+//           <div
+//             lat={Number(place.latitude)}
+//             lng={Number(place.longitude)}
+//             key={key}
+//           >
+//             <FontAwesomeIcon icon={faLocation} />
+//           </div>
+//         ))}
+//       </GoogleMapReact>
+//     </div>
+//   )
+// }
+
+// export default Map
+
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocation } from '@fortawesome/free-solid-svg-icons';
+
+const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked }) => {
   return (
     <div className='map-container'>
-        <GoogleMapReact 
-            bootstrapURLKeys={{ key: 'AIzaSyDzy3wz5yxKZv9CaxbAHUstxBLvan78zsY'}}
-            defaultCenter={coordinates}
-            center={coordinates}
-            defaultZoom={14}
-            margin={[50, 50, 50, 50]}
-            options={{ fullscreenControl: false }}
-            onChange={(e) => {
-                setCoordinates({ lat: e.center.lat, lng: e.center.lng})
-                setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw})
-            }}
-            onChildClick={''}
-            >
-        </GoogleMapReact>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyDzy3wz5yxKZv9CaxbAHUstxBLvan78zsY' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{ fullscreenControl: false }}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={''}
+      >
+            <FontAwesomeIcon 
+            icon={faLocation} 
+            position={coordinates} 
+            draggable={true} 
+            size='2xl'
+            beat
+            className='text-colombia'
+            onDragEnd={(e) => setCoordinates({ lat: e.latLng.lat(), lng: e.latLng.lng() })} 
+            />
+      </GoogleMapReact>
     </div>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
